b8f9050d354de9b921443a8f3f628360ab171ae7
mabdurrakib <mabdurrakib.dev@gmail.com>
2022-05-31 00:24:23 +0600

    tailwind crash course

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['/*.html'],
+  theme: {
+    screen: {
+      sm: '480px',
+      md: '768px',
+      md: '976px',
+      md: '1440px',
+    },
+    extend: {
+      color:{
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(229, 39%, 23%',
+        darkGrayisBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(223, 12%, 13%)',
+        veryPaleBlue: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+
+      }
+    },
+  },
+  plugins: [],
+}
